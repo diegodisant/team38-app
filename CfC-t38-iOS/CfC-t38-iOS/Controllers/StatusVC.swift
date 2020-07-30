b7edc5dcb1b9082f71e697df8a509281cddadd57
arbridev <arebrif@gmail.com>
2020-07-30 12:02:21 -0400

    feat: add qr code reader
    BREAKING CHANGE: integrate cocoa pods to project

--- CfC-t38-iOS/CfC-t38-iOS/Controllers/StatusVC.swift
+++ CfC-t38-iOS/CfC-t38-iOS/Controllers/StatusVC.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
+import QRCodeReader
 
 class StatusVC: UIViewController {
+    
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            
+            // Configure the view controller (optional)
+            $0.showTorchButton        = false
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton       = false
+            $0.showOverlayView        = true
+            $0.rectOfInterest         = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
 
     @IBOutlet weak var statusIcon: UIView!
     
@@ -42,6 +59,19 @@ class StatusVC: UIViewController {
     
     @IBAction func scanStoreCode(_ sender: UIBarButtonItem) {
         print("scan store code")
+        // Retrieve the QRCode content
+         // By using the delegate pattern
+         readerVC.delegate = self
+
+         // Or by using the closure pattern
+         readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+            print("qr value", result?.value ?? "None")
+         }
+
+         // Presents the readerVC as modal form sheet
+         readerVC.modalPresentationStyle = .formSheet
+        
+         present(readerVC, animated: true, completion: nil)
     }
     
     @IBAction func takeSurvey(_ sender: UIButton) {
@@ -63,4 +93,27 @@ class StatusVC: UIViewController {
     }
     */
 
+}
+
+// MARK: - QRCodeReaderViewController Delegate
+
+extension StatusVC: QRCodeReaderViewControllerDelegate {
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+      reader.stopScanning()
+
+      dismiss(animated: true, completion: nil)
+    }
+
+    //This is an optional delegate method, that allows you to be notified when the user switches the cameraName
+    //By pressing on the switch camera button
+    func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput) {
+        let cameraName = newCaptureDevice.device.localizedName
+        print("Switching capture to: \(cameraName)")
+    }
+
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+      reader.stopScanning()
+
+      dismiss(animated: true, completion: nil)
+    }
 }
